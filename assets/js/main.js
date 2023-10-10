6315a7e1d4c31475ef19cbe49625709885c5f6f6
RizwanSuvo99 <rizwanshuvo@gmail.com>
2023-10-10 14:59:17 +0600

    Full Completed

---
+++ assets/js/main.js
@@ -0,0 +1,10 @@
+// Owl Carousel JQuery
+$(document).ready(function () {
+  $(".owl-carousel").owlCarousel({
+    items: 4,
+    loop: true,
+    autoplay: true,
+    autoplayTimeout: 1000,
+    autoplayHoverPause: true,
+  });
+});
